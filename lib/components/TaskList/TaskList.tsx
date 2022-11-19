a41904dde2130e5aa8981c0ede8a539371db33a9
Samuel Sepesi <sepesi@bart.sk>
2022-11-19 15:01:42 +0100

    Reordering

--- lib/components/TaskList/TaskList.tsx
+++ lib/components/TaskList/TaskList.tsx
@@ -1,11 +1,23 @@
 import classNames from "classnames";
 import { TrashIcon } from "@heroicons/react/24/solid";
+import { Reorder } from "framer-motion";
+import NewTask from "../NewTask/NewTask";
+import Task from "../Task/Task";
+import { useEffect, useRef, useState } from "react";
 
 type TaskProps = {
   className?: string;
   defaultValue: string;
   handleChange: (e) => void;
   handleDelete: () => void;
+  list: any;
+  createTaskHandler: (e, id) => void;
+  updateIsFinishedHandler: (e, id) => void;
+  updateHandler: (e, id) => void;
+  deleteHandler: (id) => void;
+  updateTaskListHandler: (taskId, listId) => void;
+  otherTaskLists: any[];
+  updateTasksOrder: (listid, tasksArray) => void;
 };
 
 const TaskList = ({
@@ -13,28 +25,90 @@ const TaskList = ({
   handleChange,
   defaultValue,
   handleDelete,
+  list,
+  createTaskHandler,
+  updateIsFinishedHandler,
+  updateHandler,
+  deleteHandler,
+  updateTaskListHandler,
+  otherTaskLists,
+  updateTasksOrder,
 }: TaskProps) => {
+  const [tasks, setTasks] = useState([...list.tasks]);
+
+  useEffect(() => {
+    setTasks(list.tasks);
+  }, [list.tasks]);
+
+  const totalTasks = tasks.length;
+  const finishedTasks = tasks.filter((t) => t.isFinished).length;
+  console.log({ tasks });
+  let updateOrderTimeout = useRef(null);
+
+  const handleReorder = (newArray): void => {
+    setTasks(newArray);
+
+    if (updateOrderTimeout && updateOrderTimeout.current) {
+      clearTimeout(updateOrderTimeout.current);
+    }
+    updateOrderTimeout.current = setTimeout(() => {
+      updateTasksOrder(list.id, newArray);
+    }, 1000);
+  };
+
   return (
-    <div
-      className={classNames(
-        `flex flex-row items-center mx-auto justify-between
+    <>
+      <div
+        className={classNames(
+          `flex flex-row items-center mx-auto justify-between
          w-full max-w-xs`,
-        className
-      )}
-    >
-      <input
-        onChange={handleChange}
-        type="text"
-        name="tasklist_name"
-        defaultValue={defaultValue}
-        className="border-0 border-b-2 border-b-transparent text-2xl w-full outline-none
+          className
+        )}
+      >
+        <input
+          onChange={handleChange}
+          type="text"
+          name="tasklist_name"
+          defaultValue={defaultValue}
+          className="border-0 border-b-2 border-b-transparent text-2xl w-full outline-none
          focus-visible:border-b-primary-content transition-colors"
-        title="Edit tasklist name"
-      />
-      <button className="p-4" onClick={handleDelete}>
-        <TrashIcon className="h-6 w-6 text-error" />
-      </button>
-    </div>
+          title="Edit tasklist name"
+        />
+        <button className="p-4" onClick={handleDelete}>
+          <TrashIcon className="h-6 w-6 text-error" />
+        </button>
+      </div>
+      <div
+        className="text-center mx-auto mb-8
+    text-secondary text-opacity-60 text-sm font-semibold"
+      >
+        {finishedTasks} of {totalTasks} tasks completed
+      </div>
+      <div className="flex flex-col gap-4">
+        <NewTask handleAdd={(e) => createTaskHandler(e, list.id)} />
+        <Reorder.Group axis="y" values={tasks} onReorder={handleReorder}>
+          {tasks.map((task) => (
+            <Reorder.Item
+              key={task.id}
+              value={task}
+              className="cursor-grabbing"
+            >
+              <Task
+                handleFinishedCheck={(e) => updateIsFinishedHandler(e, task.id)}
+                handleChange={(e) => updateHandler(e, task.id)}
+                handleDelete={() => deleteHandler(task.id)}
+                handleListChange={(listId) =>
+                  updateTaskListHandler(task.id, listId)
+                }
+                defaultChecked={task.isFinished}
+                defaultValue={task.body}
+                otherTaskLists={otherTaskLists}
+              />
+            </Reorder.Item>
+          ))}
+        </Reorder.Group>
+      </div>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -102,49 +102,32 @@ const Page = ({ todoLists }) => {
     refetch();
   };
 
+  const updateTasksOrder = async (listId, tasks) => {
+    // aaa;
+    console.log("updating order");
+  };
+
   return (
     <>
       <AppLayout>
         <h1 className="text-4xl mb-10 mx-auto max-w-xs text-center">Tasker</h1>
         {data &&
           data.map((list) => {
-            const tasks = list.tasks || [];
-            const totalTasks = tasks.length;
-            const finishedTasks = tasks.filter((t) => t.isFinished).length;
             return (
               <div className="mb-6" key={list.id}>
                 <TaskList
                   defaultValue={list.name}
                   handleChange={(e) => updateListHandler(list.id, e)}
                   handleDelete={() => deleteListHandler(list.id)}
+                  list={list}
+                  createTaskHandler={createTaskHandler}
+                  updateIsFinishedHandler={updateIsFinishedHandler}
+                  updateHandler={updateHandler}
+                  deleteHandler={deleteHandler}
+                  updateTaskListHandler={updateTaskListHandler}
+                  otherTaskLists={data.filter((d) => d.id !== list.id)}
+                  updateTasksOrder={updateTasksOrder}
                 />
-                <div
-                  className="text-center mx-auto mb-8
-                  text-secondary text-opacity-60 text-sm font-semibold"
-                >
-                  {finishedTasks} of {totalTasks} tasks completed
-                </div>
-                <div className="flex flex-col gap-4">
-                  <NewTask handleAdd={(e) => createTaskHandler(e, list.id)} />
-                  {list.tasks &&
-                    list.tasks.map((task) => (
-                      <div key={task.id}>
-                        <Task
-                          handleFinishedCheck={(e) =>
-                            updateIsFinishedHandler(e, task.id)
-                          }
-                          handleChange={(e) => updateHandler(e, task.id)}
-                          handleDelete={() => deleteHandler(task.id)}
-                          handleListChange={(listId) =>
-                            updateTaskListHandler(task.id, listId)
-                          }
-                          defaultChecked={task.isFinished}
-                          defaultValue={task.body}
-                          otherTaskLists={data.filter((d) => d.id !== list.id)}
-                        />
-                      </div>
-                    ))}
-                </div>
               </div>
             );
           })}
